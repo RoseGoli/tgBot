c8fb0e973fbf84235049fa7efc540ab80a212690
RoseGoli <mytlgoli@gmail.com>
2024-05-06 08:35:09 +0330

    add new method DeleteMessages

---
+++ src/tgBot/TL/Methods/DeleteMessages.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace tgBot\TL\Methods;
+
+
+use tgBot\Telegram;
+
+/**
+ * @class DeleteMessages
+ * @description Use this method to delete a message, including service messages, with the following limitations:- A message can only be deleted if it was sent less than 48 hours ago.- A dice message in a private chat can only be deleted if it was sent more than 24 hours ago.- Bots can delete outgoing messages in private chats, groups, and supergroups.- Bots can delete incoming messages in private chats.- Bots granted can_post_messages permissions can delete outgoing messages in channels.- If the bot is an administrator of a group, it can delete any message there.- If the bot has can_delete_messages permission in a supergroup or a channel, it can delete any message there.Returns True on success.
+ *
+ * @property int|string $chat_id Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+ * @property int $message_id Identifier of the message to delete
+ *
+ *
+ * @see https://api.telegram.org/bots/api#DeleteMessages
+ */
+class DeleteMessages extends MethodDefinition implements MethodDefinitionInterface
+{
+    protected string $castsTo = 'bool';
+
+    /**
+     * @var mixed $params The value that are taken in the constructor method as method parameters.
+     */
+    public function __construct(...$params)
+    {
+        $this->params = $params;
+    }
+
+    /**
+     * @return bool
+     */
+    public function __invoke(Telegram $telegram)
+    {
+        return $this->call($telegram);
+    }
+}
